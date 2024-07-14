f08a9492269b3f729db91aa436102becdacc7a5f
Muzammil Shoukat <muzammildesigner73@gmail.com>
2024-07-14 20:27:47 +0500

    Add files via upload

---
+++ sample_program.py
@@ -0,0 +1,14 @@
+print('Muzammil Shoukat')
+x=10
+y=3
+print(x+y)
+
+
+# multi line comment
+# another line 
+# another lineeeee  
+
+print('''My Name is Zeli
+      I am Ui Ux Designer
+      hi how are you''')
+
